021f56914b7ad6b3e6ce699336343c4c1133a8bf
MichaelDiogo <michaeldiogo17@outlook.com>
2022-04-04 21:59:00 -0300

    test

---
+++ src/main/java/consultacep/ConsultacepApplication.java
@@ -0,0 +1,13 @@
+package consultacep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultacepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultacepApplication.class, args);
+	}
+
+}
